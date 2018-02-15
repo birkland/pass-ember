4b11160f15bbac57ab5ee617f90ef69e25a0220b
Aaron Birkland <apb@jhu.edu>
2018-02-15 15:52:12 -0500

    use ember for context location

--- app/serializers/fedora-jsonld.js
+++ app/serializers/fedora-jsonld.js
@@ -8,7 +8,7 @@ import { classify } from '@ember/string';
 // TODO Move configuration settings for pass to application serializer
 
 export default DS.Serializer.extend({
-    contextURI: 'http://localhost/context.jsonld',
+    contextURI: 'http://ember/context.jsonld',
     dataURI: 'https://github.com/DataConservancy/pass-ember/',
     dataPrefix: 'pass',
 
